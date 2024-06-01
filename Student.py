a5e8c303d0dc2b6ebf26792d1abcad325081003e
G016 <g.pverma716@gmail.com>
2024-06-01 22:01:37 +0530

    Get Cursor

--- Student.py
+++ Student.py
@@ -304,6 +304,7 @@ class Student:
         self.student_table.column("Photo Sample Status",width=150)
                 
         self.student_table.pack(fill=BOTH,expand=1)
+        self.student_table.bind("<ButtonRelease>",self.get_cursor)
         self.fetch_data()
 
     #Function Declaration
@@ -348,10 +349,24 @@ class Student:
         conn.close
 
     #Get Cursor
-    def get_cursor(self):
+    def get_cursor(self,event=""):
         cursor_focus=self.student_table.focus()
         content=self.student_table.item(cursor_focus)
         data=content["values"]
+
+        self.var_Branch.set(data[0]),
+        self.var_Session.set(data[1]),
+        self.var_Year.set(data[2]),
+        self.var_Semester.set(data[3]),
+        self.var_Student_Name.set(data[4]),
+        self.var_Roll_No.set(data[5]),
+        self.var_Gender.set(data[6]),
+        self.var_DOB.set(data[7]),
+        self.var_Email.set(data[8]),
+        self.var_Mobile_No.set(data[9]),
+        self.var_Photo_Sample_Status.set(data[10]),
+        self.var_Rbtn1.set(data[11])
+
             
             
 
